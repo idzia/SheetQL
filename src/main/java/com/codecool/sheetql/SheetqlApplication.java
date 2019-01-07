92fa7b0d0620c1a7d9b791271e84eabe90a8665f
idzia <ania.idzi@gmail.com>
2019-01-07 11:38:37 +0100

    Initialize project

---
+++ src/main/java/com/codecool/sheetql/SheetqlApplication.java
@@ -0,0 +1,14 @@
+package com.codecool.sheetql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheetqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SheetqlApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/codecool/sheetql/SheetqlApplicationTests.java
@@ -0,0 +1,17 @@
+package com.codecool.sheetql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SheetqlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
