6907ee2fc143c86d1a42a5bd4265469d35ae7c3d
idzia <ania.idzi@gmail.com>
2019-01-09 11:19:08 +0100

    Remove redundant public

--- src/main/java/com/codecool/sheetql/dao/Reader.java
+++ src/main/java/com/codecool/sheetql/dao/Reader.java
@@ -3,6 +3,6 @@ package com.codecool.sheetql.dao;
 
 import java.util.List;
 
-public interface Reader {
-    public List<List<String>> read(String fileName);
+public interface Reader{
+    List<List<String>> read(String fileName);
 }
